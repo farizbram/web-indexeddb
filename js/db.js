7d4bab5b73074b8bdee1f962fdd70863e675fa64
bramasta <farisbram@gmail.com>
2019-12-30 20:23:19 +0700

    Add Operation Save Article

--- js/db.js
+++ js/db.js
@@ -4,3 +4,17 @@ var dbPromised = idb.open("news-reader", 1, function(upgradeDb) {
   });
   articlesObjectStore.createIndex("post_title", "post_title", { unique: false });
 });
+
+function saveForLater(article) {
+  dbPromised
+    .then(function(db) {
+      var tx = db.transaction("articles", "readwrite");
+      var store = tx.objectStore("articles");
+      console.log(article);
+      store.add(article.result);
+      return tx.complete;
+    })
+    .then(function() {
+      console.log("Artikel berhasil di simpan.");
+    });
+}
